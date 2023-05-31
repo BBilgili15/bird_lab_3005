161adeaeab5393af62fff9e71577953769dbe549
Ben Bilgili <benbilgili@Bens-MacBook-Air.local>
2023-05-31 10:23:46 +0100

    update comments

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -9,7 +9,7 @@ const createRouter = function (collection) {
     collection
       .find()
       .toArray()
-      .then((docs) => res.json(docs))
+      .then((docs) => res.json(docs)) 
       .catch((err) => {
         console.error(err);
         res.status(500);
@@ -34,14 +34,30 @@ const createRouter = function (collection) {
     collection.insertOne(newBirdData)
     .then((result) => {
       res.json(result.ops[0])
-    })
+    }) // .ops[0] returns the first item in the array. Which is the thing we've added (index 0)
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
   })
 
   router.delete('/:id', (req, res) => {
     const id = req.params.id;
     collection.deleteOne({_id: ObjectID(id)})
-    .then((result) => res.json(result))
+    .then(() => collection.find().toArray())
+    // once we've deleted, we're getting the state of things back. Find() does it gradually. ToArray bangs them all in a list 
+    // ToArray not good code if we have massive data. Find() would be useful 
+    .then((result) => res.json(result)) 
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
   })
+
+
+  // best to have specific error codes in future rather than just 'err 500'
 
   return router;
 };
